59a75bf79377a24f9fee4026eeb797c3aed04a74
johan <johancfuentes03@gmail.com>
2023-12-30 12:36:30 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        IElectronicDevice TV = TVRemote.GetDevice();
+
+        PowerButton powerButton = new PowerButton(TV);
+
+        powerButton.Execute();
+        powerButton.Undo();
+
+    }
+}
---
+++ Television.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Television : IElectronicDevice
+    {
+        public int Volume { get; set; }
+
+        public void Off()
+        {
+            Console.WriteLine("The  TV is Off");
+            
+        }
+
+        public void On()
+        {
+            Console.WriteLine("The  TV is On");
+
+        }
+
+        public void VolumeUp()
+        {
+            if (Volume < 100)
+            {
+                Volume++;
+                Console.WriteLine("The TV Volumne is {0}", Volume);
+            }
+        }
+
+        public void VolumeDown()
+        {
+            if (Volume > 0)
+            {
+                Volume--;
+                Console.WriteLine("The TV Volumne is {0}", Volume);
+            }
+        }
+    }
+    
+}
